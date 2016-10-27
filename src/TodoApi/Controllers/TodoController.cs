beb4404694e4e8717f4ab0b88c62913d94e02386
AlexL70 <alexander.levinson.70@gmail.com>
2016-10-27 22:13:55 +0300

    Implement "PATCH /api/todo/{id}" endpoint

--- src/TodoApi/Controllers/TodoController.cs
+++ src/TodoApi/Controllers/TodoController.cs
@@ -49,7 +49,7 @@ namespace TodoApi.Controllers
         [HttpPut("{id}")]
         public IActionResult Update(string id, [FromBody] TodoItem item)
         {
-            if (item == null || string.IsNullOrEmpty(id) || item.Key != id)
+            if (item == null || item.Key != id)
             {
                 return BadRequest();
             }
@@ -62,6 +62,27 @@ namespace TodoApi.Controllers
 
             TodoItems.Update(item);
             return new NoContentResult();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update([FromBody] TodoItem item, string id)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            item.Key = todo.Key;
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+
         }
     }
 }
