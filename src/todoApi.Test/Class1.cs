0e4287db287313b43c4c72e3c2da6074cf9037e9
AlexL70 <alexander.levinson.70@gmail.com>
2016-10-31 12:47:56 +0200

    add test project and setup NUnit test runner

---
+++ src/todoApi.Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace todoApi.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        [Test]
+        public void TestTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
